781f56bad9a413778c12ae6806ff5185521ede20
Gabriel <gabriel_henrique2805@hotmail.com>
2023-03-15 20:27:41 -0300

    insert de telefone

--- src/main/java/executavel/ExecutavelTelefonia.java
+++ src/main/java/executavel/ExecutavelTelefonia.java
@@ -1,7 +1,12 @@
 package executavel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.dao.telefonia.EnderecoDAO;
+import model.dao.telefonia.TelefoneDAO;
 import model.vo.telefonia.Endereco;
+import model.vo.telefonia.Telefone;
 
 public class ExecutavelTelefonia {
 
@@ -12,17 +17,29 @@ public class ExecutavelTelefonia {
 		EnderecoDAO salvadorDeEnderecos = new EnderecoDAO();
 		salvadorDeEnderecos.inserir(endereco1);
 		
+		Telefone telefone = new Telefone("48","96348863",true,true);
+		TelefoneDAO.inserir(telefone);
+		
 		if(endereco1.getId() != null) {
 			System.out.println("Novo endereço cadastrado");
 		}else {
 			System.out.println("Erro ao cadastrar endereço");
 		}
 		
-//		List<Telefone> telefonesDoSocrates = new ArrayList<Telefone>();
-//		Telefone telefone1 = new Telefone("48", "32328888", true, false);
-//		telefonesDoSocrates.add(telefone1);
-//		telefonesDoSocrates.add(new Telefone("48", "98881234", true, true));
-//		
+		if(telefone.getId() != null)
+		{
+			System.out.println("Telefone cadastrado com sucesso! :)");
+		}
+		else
+		{
+			System.out.println("Erro ao cadastrar o telefone :(");
+		}
+		
+		List<Telefone> telefonesDoSocrates = new ArrayList<Telefone>();
+		telefonesDoSocrates.add(telefone);
+		
+		System.out.println(telefonesDoSocrates.get(0));
+		
 //		Cliente pele = new Cliente("Edson Arantes", "11122233344", null, true, endereco1);
 //		Cliente socrates = new Cliente("Sócrates Brasileiro", "33322233344", telefonesDoSocrates, true, endereco1);
 //		
--- src/main/java/model/dao/telefonia/TelefoneDAO.java
+++ src/main/java/model/dao/telefonia/TelefoneDAO.java
@@ -2,6 +2,7 @@ package model.dao.telefonia;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import model.dao.Banco;
@@ -13,15 +14,39 @@ public class TelefoneDAO {
 	{
 		try 
 		{
-			String insert = "insert into telefone('DDD','NUMERO','ATIVO','MOVEL','ID_CLIENTE')+"
-					+ "values ('?','?','?','?','?')";
+			String insert = "insert into telefone(DDD,NUMERO,ATIVO,MOVEL) "
+					+ "values (?,?,?,?)";
 			Connection conexao = Banco.getConnection();
 			PreparedStatement query = Banco.getPreparedStatementWithPk(conexao, insert);
 			
 			query.setString(1, telefone.getDdd());
+			query.setString(2, telefone.getNumero());
+			query.setBoolean(3, telefone.isAtivo());
+			query.setBoolean(4, telefone.isMovel());
+			if(telefone.getIdCliente() != null)
+			{
+				query.setInt(5, telefone.getIdCliente());
+			}
+			
+			int affectedColumns = query.executeUpdate();
+			ResultSet keys = query.getGeneratedKeys();
+			
+			if(affectedColumns > 0)
+			{
+				if(keys.next())
+				{
+					telefone.setId(keys.getInt(1));
+				}
+			}
+			
+			Banco.closeResultSet(keys);
+			Banco.closePreparedStatement(query);
+			Banco.closeConnection(conexao);
+			
 		} catch(SQLException e)
 		{
-			
+			System.out.println("Erro no método inserir da classe TelefoneDAO");
+			System.out.println(e.getMessage());
 		}
 	}
 }
